3056622c4bb21fc3045ef6dd38521da79a8a5d29
GNamimates <81744903+GNamimates@users.noreply.github.com.>
2025-02-23 14:47:23 +0800

    added category loading to settings

--- src/panels/settings/settings_ui.gd
+++ src/panels/settings/settings_ui.gd
@@ -2,6 +2,9 @@ extends Control
 
 @onready var CategoriesButtons = $HBoxContainer/CategoriesPanel/ScrollContainer/CategoriesButtons
 @onready var OptionsButtons = $HBoxContainer/OptionsPanel/ScrollContainer/OptionsButtons
+@onready var CategoryLabel = $HBoxContainer/OptionsPanel/CategoryLabel
+
+@onready var CategoryButton = preload("res://panels/settings/category_button.tscn")
 
 enum OPTION_TYPE {
 	BOOLEAN,
@@ -11,15 +14,32 @@ enum OPTION_TYPE {
 	COLOR,
 	KEYBIND,
 }
-
-
 @export var options: Dictionary[OPTION_TYPE,PackedScene]
+var current_category: String = "": set = set_current_category
+
 
 func _ready():
+	reload_category_list()
+	set_current_category("")
+
+
+func set_current_category(category: String) -> void:
 	for child in OptionsButtons.get_children(): child.queue_free()
-	for category in Settings.get_categories():
+	if Settings.has_category(category):
+		category = category
+		CategoryLabel.text = category
 		for option in Settings.get_category_options(category):
 			var option_instance = options[option.type].instantiate()
 			option_instance.option_name = option.name
 			OptionsButtons.add_child(option_instance)
-		return
+	else:
+		CategoryLabel.text = "..."
+		category = ""
+
+
+func reload_category_list():
+	for child in CategoriesButtons.get_children(): child.queue_free()
+	for category in Settings.get_categories():
+		var category_instance = CategoryButton.instantiate()
+		category_instance.text = category
+		CategoriesButtons.add_child(category_instance)
