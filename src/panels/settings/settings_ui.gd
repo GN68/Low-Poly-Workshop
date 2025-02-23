02e767cd0d05361e5994ecd47ac02a44a0885011
GNamimates <81744903+GNamimates@users.noreply.github.com.>
2025-02-23 14:28:54 +0800

    added primitive settings UI loader

--- src/panels/settings/settings_ui.gd
+++ src/panels/settings/settings_ui.gd
@@ -1,3 +1,25 @@
 extends Control
 
-@export var options: Dictionary[String,SettingsOption]
+@onready var CategoriesButtons = $HBoxContainer/CategoriesPanel/ScrollContainer/CategoriesButtons
+@onready var OptionsButtons = $HBoxContainer/OptionsPanel/ScrollContainer/OptionsButtons
+
+enum OPTION_TYPE {
+	BOOLEAN,
+	STRING,
+	NUMBER,
+	LIST,
+	COLOR,
+	KEYBIND,
+}
+
+
+@export var options: Dictionary[OPTION_TYPE,PackedScene]
+
+func _ready():
+	for child in OptionsButtons.get_children(): child.queue_free()
+	for category in Settings.get_categories():
+		for option in Settings.get_category_options(category):
+			var option_instance = options[option.type].instantiate()
+			option_instance.option_name = option.name
+			OptionsButtons.add_child(option_instance)
+		return
