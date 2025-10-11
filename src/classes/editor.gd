c30d5f94e57f744ae3152def32b78b2d0bbd499e
GN68 <81744903+GN68@users.noreply.github.com>
2025-10-11 21:16:11 +0800

    added basic editor APIs

--- src/classes/editor.gd
+++ src/classes/editor.gd
@@ -1,3 +1,23 @@
 extends Node
+# The Editor global
+
+var workspaces : Array[Workspace] = []
+var current_workspace : Workspace: set = set_workspace
 
 
+func _ready() -> void:
+	set_workspace(Workspace.new())
+
+
+func set_workspace(new_workspace):
+	if current_workspace: remove_child(current_workspace)
+	current_workspace = new_workspace
+	if !is_node_ready(): await ready
+	add_child(current_workspace)
+
+
+func create_mesh():
+	assert(current_workspace is Workspace, "Current Workspace does not exist or is not a Workspace")
+	
+	var object = LPWMesh.new()
+	current_workspace.add_object(object)
