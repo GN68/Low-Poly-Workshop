c7603db1bf48c4e359aed68d3fcb5c42b6fc1b60
GN68 <81744903+GN68@users.noreply.github.com>
2025-10-13 00:24:56 +0800

    added object parent related events (all unused)

--- src/classes/workspace.gd
+++ src/classes/workspace.gd
@@ -3,11 +3,26 @@ class_name Workspace
 
 @export var source_path : String
 @export var unsaved_changes : bool
-@export var objects : Array
-# this is the root instead of the node itself to avoid being able to get nodes in the editor.
+
+signal object_moved(object: LPWObject, new_parent: LPWObject)
+signal object_removed(object: LPWObject)
+signal object_added(object: LPWObject)
+
+var scene = preload("res://debug/node_tree.tscn")
 
 
-## Add an object to the workspace
-func add_object(object : LPWObject):
-	objects.append(object)
-	add_child(object)
+func _ready() -> void:
+	add_child(scene.instantiate())
+
+
+## Checks if the given object is inside the workspace
+func is_inside_workspace(object: LPWObject):
+	return true # TODO: make use of getPath() to check if the object is inside.
+
+
+## Moves the given object to the new parent.
+func move_object(object: LPWObject, new_parent: LPWObject):
+	if object.get_parent():
+		object.get_parent().remove_child(object)
+	new_parent.add_child(object)
+	object_moved.emit(object, new_parent)
