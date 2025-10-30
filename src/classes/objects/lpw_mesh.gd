48345caf942b19f2b4db8f57c56433d328bd0e57
GN68 <81744903+GN68@users.noreply.github.com>
2025-10-30 16:14:13 +0800

    added more mesh APIs

--- src/classes/objects/lpw_mesh.gd
+++ src/classes/objects/lpw_mesh.gd
@@ -4,19 +4,32 @@ class_name LPWMesh
 @export var mesh : Mesh : set = set_mesh
 var mesh_instance : MeshInstance3D
 
+
 func set_mesh(new_mesh : Mesh):
 	mesh = new_mesh
 	mesh_instance.mesh = mesh
+
 
 func _init():
 	mesh_instance = MeshInstance3D.new()
 	add_child(mesh_instance)
 	mesh = BoxMesh.new()
+	name = "Mesh"
+
+
+func get_icon_name() -> String:
+	return "mesh"
 
 
 func pack_data(data) -> Dictionary:
-	return {}
+	return {
+		position = position,
+		rotation = rotation,
+		scale = scale,
+	}
 
 
 func unpack_data(data) -> void:
-	pass
+	position = data.position
+	rotation = data.rotation
+	scale = data.scale
