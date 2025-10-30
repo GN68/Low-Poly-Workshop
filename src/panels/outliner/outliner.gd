96f23c72298c1c484ed60d3628c9d268d46328fc
GN68 <81744903+GN68@users.noreply.github.com>
2025-10-30 16:16:17 +0800

    outliner fixes - fixed outliner giving a horrible name when two nodes share the same name - fixed being able to drag the same node to the same parent which caused the node to think the same node with the same name already exists and therefore gives itself a unique name

--- src/panels/outliner/outliner.gd
+++ src/panels/outliner/outliner.gd
@@ -21,10 +21,12 @@ func update_tree():
 ## Generates the tree recursively
 func _tree_update(node: Node = LPWEditor.current_workspace, parent: TreeItem = null):
 	for child in node.get_children():
-		var item: TreeItem = create_item(parent)
-		item.set_text(0, child.name)
-		item.set_metadata(0,child)
-		_tree_update(child, item)
+		if child is LPWObject:
+			var item: TreeItem = create_item(parent)
+			item.set_text(0, child.name)
+			item.set_icon(0, get_theme_icon(child.get_icon_name(),"Icons"))
+			item.set_metadata(0,child)
+			_tree_update(child, item)
 
 
 func _drag_started(data: Variant):
@@ -58,8 +60,20 @@ func _drop_data(at_position: Vector2, data: Variant) -> void:
 	LPWEditor.drag_ended.emit(data)
 	var item: TreeItem = get_item_at_position(at_position)
 	if item:
-		var node: Node = item.get_metadata(0)
+		var new_parent_node: Node = item.get_metadata(0)
 		if data is Node:
-			if data != node and !data.is_ancestor_of(node):
-				data.reparent(node)
+			if (data != new_parent_node # if new parent is not itself
+			and !data.is_ancestor_of(new_parent_node) # new parent is not an ancestor of the node.
+			and data.get_parent() != new_parent_node): # new parent is not the parent of the node already
+				# the node with the given name already exists
+				# find a name that does not exist
+				if new_parent_node.has_node(NodePath(data.name)):
+					var no_number_name = data.name.rstrip("0123456789")
+					for i in range(2,1000):
+						var new_name = no_number_name + str(i)
+						if !new_parent_node.has_node(NodePath(new_name)):
+							data.name = new_name
+							break
+				
+				data.reparent(new_parent_node)
 				update_tree()
